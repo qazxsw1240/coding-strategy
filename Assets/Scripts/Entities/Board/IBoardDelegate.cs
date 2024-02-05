fc6a4f75d6b21eb05b7e0a27da5c486d2c5b69b4
qazxsw1240 <qazxsw010528@gmail.com>
2024-02-05 10:39:28 +0900

    Update IBoardDelegate interface

--- Assets/Scripts/Entities/Board/IBoardDelegate.cs
+++ Assets/Scripts/Entities/Board/IBoardDelegate.cs
@@ -11,11 +11,7 @@ namespace CodingStrategy.Entities.Board
     {
         public abstract ITile this[Coordinate coordinate] { get; set; }
 
-        public abstract ITile[,] Tiles { get; set; }
-
         public abstract IList<IRobot> Robots { get; }
-
-        public abstract bool Move(IRobotDelegate robot, Coordinate coordinate);
 
         public abstract bool Place(IRobotDelegate robot, Coordinate coordinate);
 
