7a82d3d16b8f883cc517a42750bbeea9d06958c9
qazxsw1240 <qazxsw010528@gmail.com>
2024-02-21 00:22:06 +0900

    Add docs in IStatement

--- Assets/Scripts/Entities/Runtime/IStatement.cs
+++ Assets/Scripts/Entities/Runtime/IStatement.cs
@@ -3,8 +3,14 @@
 
 namespace CodingStrategy.Runtime
 {
+    /// <summary>
+    /// ExecutorQueue가 실행할 명령문 인스턴스입니다.
+    /// </summary>
     public interface IStatement
     {
+        /// <summary>
+        /// 명령문을 실행합니다.
+        /// </summary>
         public abstract void Execute();
     }
 }
