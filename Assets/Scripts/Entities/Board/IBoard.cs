2687354e25f5ee1523b0bc9bef24c3bb49c5b45e
qazxsw1240 <qazxsw010528@gmail.com>
2024-03-13 22:34:50 +0900

    Update IBoard

--- Assets/Scripts/Entities/Board/IBoard.cs
+++ Assets/Scripts/Entities/Board/IBoard.cs
@@ -5,9 +5,32 @@ namespace CodingStrategy.Entities.Board
 {
     using System.Collections.Generic;
     using CodingStrategy.Entities.Robot;
+    using UnityEngine.Events;
 
     public interface IBoard
     {
         public abstract IReadOnlyList<IRobot> Robots { get; }
+
+        public abstract bool Add(IRobot robotDelegate, Coordinate position, RobotDirection direction);
+
+        public abstract bool Remove(IRobot robotDelegate);
+
+        public abstract Coordinate GetPosition(IRobotDelegate robotDelegate);
+
+        public abstract RobotDirection GetDirection(IRobotDelegate robotDelegate);
+
+        public abstract bool Place(IRobotDelegate robotDelegate, Coordinate position);
+
+        public abstract bool Rotate(IRobotDelegate robotDelegate, RobotDirection direction);
+
+        public abstract ITileDelegate[,] AsArray();
+
+        public abstract UnityEvent<IRobot> OnRobotAdd { get; }
+
+        public abstract UnityEvent<IRobot> OnRobotRemove { get; }
+
+        public abstract UnityEvent<IRobot, Coordinate, Coordinate> OnRobotChangePosition { get; }
+
+        public abstract UnityEvent<IRobot, RobotDirection, RobotDirection> OnRobotChangeDirection { get; }
     }
 }
