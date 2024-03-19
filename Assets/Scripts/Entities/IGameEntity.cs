3571684d4e27fb30a0bed67e8e23ea853359ae6a
qazxsw1240 <qazxsw010528@gmail.com>
2024-03-20 08:10:09 +0900

    Update IGameEntity

--- Assets/Scripts/Entities/IGameEntity.cs
+++ Assets/Scripts/Entities/IGameEntity.cs
@@ -1,13 +1,17 @@
 #nullable enable
 
 
-using System;
-
 namespace CodingStrategy.Entities
 {
-    public interface IGameEntity
+    using System;
+
+    public interface IGameEntity : IComparable<IGameEntity>
     {
+        public abstract string Id { get; }
+
         public TEntity As<TEntity>() where TEntity : class, IGameEntity
             => this as TEntity ?? throw new InvalidCastException();
+
+        public new int CompareTo(IGameEntity other) => Id.CompareTo(other.Id);
     }
 }
