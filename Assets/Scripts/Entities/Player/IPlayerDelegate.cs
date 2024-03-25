fa9a3d4d4c7ec6d012bb84f038edd41f62ffc6ee
es-e2b <ensu1997@naver.com>
2024-03-26 01:11:44 +0900

    Update IPlayerDelegate
    Change data type of Id field from int to string

--- Assets/Scripts/Entities/Player/IPlayerDelegate.cs
+++ Assets/Scripts/Entities/Player/IPlayerDelegate.cs
@@ -6,7 +6,7 @@ namespace CodingStrategy.Entities.Player
     using CodingStrategy.Entities.Robot;
     public interface IPlayerDelegate
     {
-        public abstract int Id { get; }
+        public abstract string Id { get; }
         public abstract int HealthPoint { get; set; }
         public abstract int Level { get; set; }    
         public abstract int Exp { get; set; }
