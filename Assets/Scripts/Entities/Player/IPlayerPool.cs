ad7eb02ad24ee30f288d41a0eff5771bfb92e9a6
es-e2b <ensu1997@naver.com>
2024-04-01 01:27:02 +0900

    Update IPlayerPool
    Removed the previously inherited interface.

--- Assets/Scripts/Entities/Player/IPlayerPool.cs
+++ Assets/Scripts/Entities/Player/IPlayerPool.cs
@@ -1,10 +1,19 @@
-#nullable enable
-
-
 namespace CodingStrategy.Entities.Player
 {
-    public interface IPlayerPool : IObjectPool<IPlayerDelegate>
+    using System.Collections.Generic;
+    /// <summary>
+    /// 플레이어 풀입니다. 플레이어의 아이디로 해당 플레이어의 상태에 접근할 수 있습니다.
+    /// </summary>
+    public interface IPlayerPool
     {
-        
+        /// <summary>
+        /// 플레이어 아이디를 키로, 플레이어의 상태를 값으로 저장하는 딕셔너리입니다.
+        /// </summary>
+        public abstract IDictionary<string, IPlayerDelegate> PlayerPool { get; }
+
+        /// <summary>
+        /// 플레이어 아이디로 해당 플레이어의 상태에 접근합니다.
+        /// </summary>
+        public abstract IPlayerDelegate this[string id] { get; }
     }
 }
