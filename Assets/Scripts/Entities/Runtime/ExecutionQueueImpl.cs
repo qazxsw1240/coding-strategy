5463c14233df6c6300f45dbfc90b2048b9e8c78d
qazxsw1240 <qazxsw010528@gmail.com>
2024-04-01 17:41:51 +0900

    Update IExecutionQueue

--- Assets/Scripts/Entities/Runtime/ExecutionQueueImpl.cs
+++ Assets/Scripts/Entities/Runtime/ExecutionQueueImpl.cs
@@ -36,6 +36,8 @@ namespace CodingStrategy.Runtime
 
         public IStatement Dequeue() => _statements.Dequeue();
 
+        public bool Remove(IStatement item) => throw new NotImplementedException();
+
         public bool TryDequeue(out IStatement statement) => _statements.TryDequeue(out statement);
 
         public void Clear() => _statements.Clear();
@@ -45,5 +47,6 @@ namespace CodingStrategy.Runtime
         public IEnumerator<IStatement> GetEnumerator() => _statements.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
     }
 }
--- Assets/Scripts/Entities/Runtime/IExecutionQueue.cs
+++ Assets/Scripts/Entities/Runtime/IExecutionQueue.cs
@@ -7,7 +7,7 @@ namespace CodingStrategy.Runtime
     using System.Collections;
     using System.Collections.Generic;
 
-    public interface IExecutionQueue : IReadOnlyCollection<IStatement>
+    public interface IExecutionQueue : ICollection<IStatement>
     {
         public abstract void Enqueue(IStatement statement);
 
