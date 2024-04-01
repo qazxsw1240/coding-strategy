faa1e03f9ad57cd2e273b015f271bcf8a1a8d210
qazxsw1240 <qazxsw010528@gmail.com>
2024-04-01 18:52:48 +0900

    Add MoveStatement

---
+++ Assets/Scripts/Entities/Runtime/Statement/MoveStatement.cs
@@ -0,0 +1,45 @@
+#nullable enable
+
+
+namespace CodingStrategy.Runtime.Statement
+{
+    using System;
+    using CodingStrategy.Entities.Robot;
+    using UnityEngine;
+
+    public class MoveStatement : IStatement
+    {
+        private readonly IRobotDelegate _robotDelegate;
+        private readonly int _direction;
+        private bool _isEdge;
+
+        public MoveStatement(IRobotDelegate robotDelegate, int direction, bool isEdge = false)
+        {
+            if (direction != 1 && direction != -1)
+            {
+                throw new ArgumentException();
+            }
+            _robotDelegate = robotDelegate;
+            _direction = direction;
+            _isEdge = isEdge;
+        }
+
+        public void Execute()
+        {
+            Debug.LogFormat("Try to move to {0}", _direction);
+            _isEdge = !_robotDelegate.Move(_direction);
+            if (_isEdge)
+            {
+                throw new ExecutionException();
+            }
+        }
+
+        public IStatement Reverse
+        {
+            get
+            {
+                return _isEdge ? this : new MoveStatement(_robotDelegate, -1, _isEdge);
+            }
+        }
+    }
+}
