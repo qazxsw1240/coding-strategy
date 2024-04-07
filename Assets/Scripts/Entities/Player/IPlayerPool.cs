b39f9d0dd8b02ebb7a4471dc614a68bdbbc5f3dc
es-e2b <ensu1997@naver.com>
2024-04-08 03:18:45 +0900

    Update IPlayerPool

--- Assets/Scripts/Entities/Player/IPlayerPool.cs
+++ Assets/Scripts/Entities/Player/IPlayerPool.cs
@@ -10,10 +10,20 @@ namespace CodingStrategy.Entities.Player
         /// 플레이어 아이디를 키로, 플레이어의 상태를 값으로 저장하는 딕셔너리입니다.
         /// </summary>
         public abstract IDictionary<string, IPlayerDelegate> PlayerPool { get; }
-
         /// <summary>
         /// 플레이어 아이디로 해당 플레이어의 상태에 접근합니다.
         /// </summary>
         public abstract IPlayerDelegate this[string id] { get; }
+        /// <summary>
+        /// 플레이어 풀에 플레이어를 추가합니다.
+        /// </summary>
+        /// <param name="id">플레이어 아이디입니다.</param>
+        /// <param name="player">플레이어 정보 대리자입니다.</param>
+        public abstract void Add(string id, IPlayerDelegate player);
+        /// <summary>
+        /// 플레이어 풀에서 특정 플레이어를 제거합니다.
+        /// </summary>
+        /// <param name="id">제거할 플레이어의 아이디입니다.</param>
+        public abstract void Remove(string id);
     }
 }
