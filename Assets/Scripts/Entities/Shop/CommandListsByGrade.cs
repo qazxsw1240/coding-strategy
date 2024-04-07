dad8e02d2a5fd1464b72d5f54f4e959d3c29d426
es-e2b <ensu1997@naver.com>
2024-04-08 02:53:33 +0900

    Update CommandListsByGrade
    Added description

--- Assets/Scripts/Entities/Shop/CommandListsByGrade.cs
+++ Assets/Scripts/Entities/Shop/CommandListsByGrade.cs
@@ -1,8 +1,12 @@
 namespace CodingStrategy.Entities.Shop
 {
     using System;
+    /// <summary>
+    /// 등급별 명령어 리스트입니다.
+    /// </summary>
     public class CommandListsByGrade
     {
+        // 등급별 명령어 리스트입니다.
         private readonly CommandListImpl _commandListGrade1;
         private readonly CommandListImpl _commandListGrade2;
         private readonly CommandListImpl _commandListGrade3;
@@ -16,7 +20,12 @@ namespace CodingStrategy.Entities.Shop
             _commandListGrade4 = new CommandListImpl();
             _commandListGrade5 = new CommandListImpl();
         }
-
+        /// <summary>
+        /// 특정 등급의 명령어 리스트를 반환합니다.
+        /// </summary>
+        /// <param name="grade">반환 받을 리스트의 등급입니다.</param>
+        /// <returns>명령어 리스트를 반환합니다.</returns>
+        /// <exception cref="ArgumentException">잘못된 등급일 경우 예외를 발생시킵니다.</exception>
         public CommandListImpl this[int grade]
         {
             get
