93aeec54b5a049a45281f1ae7b6c0b9bfa392bfe
es-e2b <ensu1997@naver.com>
2024-04-08 03:21:14 +0900

    Update IRequiredExp

--- Assets/Scripts/Entities/Player/IRequiredExp.cs
+++ Assets/Scripts/Entities/Player/IRequiredExp.cs
@@ -9,7 +9,7 @@ namespace CodingStrategy.Entities.Player
         /// 현재 레벨에서 다음 레벨까지 필요한 총 경험치를 반환합니다.
         /// </summary>
         /// <param name="currentLevel">현재 레벨을 매개 변수로 입력합니다.</param>
-        /// <returns>현재 레벨에 해당하는 총 경험치를 반환합니다.</returns>
+        /// <returns>다음 레벨까지 필요한 총 경험치를 반환합니다.</returns>
         public abstract int this[int currentLevel] { get; }
     }
 }
