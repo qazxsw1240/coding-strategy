694407c09547715cf6fd157c3ef97f0a4daac855
qazxsw1240 <qazxsw010528@gmail.com>
2024-04-19 19:13:49 +0900

    Add RuntimeExecutor

---
+++ Assets/Scripts/Entities/Runtime/RuntimeExecutor.cs
@@ -0,0 +1,81 @@
+#nullable enable
+
+
+namespace CodingStrategy.Runtime
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using CodingStrategy.Entities;
+    using CodingStrategy.Entities.Board;
+    using CodingStrategy.Entities.Robot;
+    using Photon.Pun;
+    using Photon.Realtime;
+
+    public class RuntimeExecutor : ILifeCycle
+    {
+        private readonly IBoardDelegate _boardDelegate;
+        private readonly int _countdown;
+        private readonly ExecutionQueuePool _executionQueuePool;
+        private readonly IList<IExecutionValidator> _validators;
+
+        private int _currentCountdown;
+
+        public RuntimeExecutor(IBoardDelegate boardDelegate, int countdown)
+        {
+            _boardDelegate = boardDelegate;
+            _countdown = countdown;
+            _executionQueuePool = new ExecutionQueuePool();
+            _validators = new List<IExecutionValidator>();
+
+            _currentCountdown = 0;
+        }
+
+        public void Initialize()
+        {
+            // Place robots
+            // Place bad sectors
+            // Inject abnormality code
+            // Initialize ExecutionQueue
+            _executionQueuePool.Clear();
+            // Initialize players' data
+        }
+
+        public bool MoveNext()
+        {
+            // Check deadlock
+            return _executionQueuePool.Count != 0 && ++_currentCountdown <= _countdown;
+        }
+
+        public bool Execute()
+        {
+            // Check deadlock
+            if (_executionQueuePool.Count == 0)
+            {
+                return false;
+            }
+
+            foreach ((IRobotDelegate robotDelegate, IExecutionQueue executionQueue) in _executionQueuePool)
+            {
+                IAlgorithm algorithm = robotDelegate.Algorithm;
+                int capacity = algorithm.Count;
+                if (capacity == 0)
+                {
+                    continue;
+                }
+                ICommand command = algorithm[_currentCountdown % capacity];
+                // execute command
+            }
+
+            return true;
+        }
+
+        public void Terminate()
+        {
+            foreach (IExecutionQueue executionQueue in _executionQueuePool.Values)
+            {
+                executionQueue.Clear();
+            }
+            _executionQueuePool.Clear();
+        }
+    }
+}
