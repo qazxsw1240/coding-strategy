60ede5fd52bc5e5db3943ec60ea2900d156d76f7
qazxsw1240 <qazxsw010528@gmail.com>
2024-04-19 18:52:32 +0900

    Add docs

--- Assets/Scripts/Entities/Board/ICellDelegate.cs
+++ Assets/Scripts/Entities/Board/ICellDelegate.cs
@@ -7,10 +7,19 @@ namespace CodingStrategy.Entities.Board
     using CodingStrategy.Entities.BadSector;
     using CodingStrategy.Entities.Robot;
 
+    /// <summary>
+    /// 셀의 정보를 담고 있는 딜리게이트입니다.
+    /// </summary>
     public interface ICellDelegate
     {
+        /// <summary>
+        /// 셀에 설치된 배드섹터입니다. 설치된 배드섹터가 없으면 null이 반환됩니다.
+        /// </summary>
         public abstract IBadSectorDelegate? BadSector { get; set; }
 
+        /// <summary>
+        /// 셀에 배치된 로봇 컬렉션입니다.
+        /// </summary>
         public abstract ISet<IRobotDelegate> Robot { get; set; }
     }
 }
