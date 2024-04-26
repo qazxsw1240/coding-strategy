725e981c498f006df2204373c09a20cd884cef0c
qazxsw1240 <qazxsw010528@gmail.com>
2024-04-26 18:05:07 +0900

    Add StaticValidator

---
+++ Assets/Scripts/Entities/Runtime/Validator/StaticValidator.cs
@@ -0,0 +1,13 @@
+﻿using CodingStrategy.Entities.Board;
+
+namespace CodingStrategy.Entities.Runtime.Validator
+{
+    public class StaticValidator: IExecutionValidator
+    {
+        public bool IsValid(IBoardDelegate boardDelegate)
+        {
+            // TODO: 미구현 상태
+            return true;
+        }
+    }
+}
