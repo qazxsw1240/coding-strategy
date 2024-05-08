3900134eb549f0893e231f117e90c9456a865466
qazxsw1240 <qazxsw010528@gmail.com>
2024-05-09 04:23:33 +0900

    Add LifeCycleMonoBehaviourBase

---
+++ Assets/Scripts/Entities/LifeCycleMonoBehaviourBase.cs
@@ -0,0 +1,63 @@
+﻿#nullable enable
+
+
+namespace CodingStrategy.Entities
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public abstract class LifeCycleMonoBehaviourBase : MonoBehaviour
+    {
+        private Coroutine? _coroutine;
+
+        protected LifeCycleMonoBehaviourBase(ILifeCycle lifeCycle)
+        {
+            LifeCycle = lifeCycle;
+        }
+
+        public void Start()
+        {
+            _coroutine = StartCoroutine(StartLifeCycleCoroutine());
+        }
+
+        public bool HasCoroutine => _coroutine != null;
+
+        public ILifeCycle LifeCycle { get; }
+
+        protected abstract IEnumerator OnAfterInitialization();
+
+        protected abstract IEnumerator OnBeforeExecution();
+
+        protected abstract IEnumerator OnAfterFailExecution();
+
+        protected abstract IEnumerator OnAfterExecution();
+
+        protected abstract IEnumerator OnAfterTermination();
+
+        private IEnumerator StartLifeCycleCoroutine()
+        {
+            LifeCycle.Initialize();
+
+            yield return OnAfterInitialization();
+
+            while (LifeCycle.MoveNext())
+            {
+                yield return OnBeforeExecution();
+
+                if (!LifeCycle.Execute())
+                {
+                    yield return OnAfterFailExecution();
+                    break;
+                }
+
+                yield return OnAfterExecution();
+            }
+
+            LifeCycle.Terminate();
+
+            yield return OnAfterTermination();
+
+            _coroutine = null;
+        }
+    }
+}
