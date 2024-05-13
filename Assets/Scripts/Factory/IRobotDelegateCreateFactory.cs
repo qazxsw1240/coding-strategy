fe5fa23890c852361e225d7bd0df4fe8235ff43d
qazxsw1240 <qazxsw010528@gmail.com>
2024-05-14 03:00:47 +0900

    Add IRobotDelegateCreateFactory

---
+++ Assets/Scripts/Factory/IRobotDelegateCreateFactory.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+
+namespace CodingStrategy.Factory
+{
+    using Entities.Robot;
+
+    public interface IRobotDelegateCreateFactory
+    {
+        public abstract IRobotDelegateCreateStrategy Strategy { get; }
+
+        public abstract IRobotDelegate Build();
+    }
+}
