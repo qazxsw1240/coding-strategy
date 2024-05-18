204c7f7ccb8d81ccae29aa7d5631c0ffaeed2276
gpfl0712 <gpfl0712@gmail.com>
2024-05-19 01:21:42 +0900

    Update ShopUi.cs

--- Assets/Scripts/UI/Shop/ShopUi.cs
+++ Assets/Scripts/UI/Shop/ShopUi.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using CodingStrategy.Entities;
 
 public class ShopUi : MonoBehaviour
 {
@@ -11,6 +13,15 @@ public class ShopUi : MonoBehaviour
         Transform _parent = gameObject.transform.Find("SelectedCommandList");
 		GameObject _object = Instantiate(plusIcon, _parent);
         _object.transform.SetSiblingIndex(_parent.childCount - 2);
+    }
+
+    public void SetShopCommandList(ICommand[] list)
+    {
+        foreach (ICommand command in list)
+        {
+            // command.Id;
+            // command.Info;
+        }
     }
 
     // Start is called before the first frame update
