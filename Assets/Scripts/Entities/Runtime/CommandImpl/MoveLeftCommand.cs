81be59777b7c19394b58f5760c75dba53a50376d
es-e2b <ensu1997@naver.com>
2024-05-18 19:01:13 +0900

    Add MoveLeftCommand

---
+++ Assets/Scripts/Entities/Runtime/CommandImpl/MoveLeftCommand.cs
@@ -0,0 +1,45 @@
+#nullable enable
+
+
+namespace CodingStrategy.Entities.Runtime.CommandImpl
+{
+    using System.Collections.Generic;
+    using CodingStrategy.Entities.Robot;
+    using Statement;
+
+    public class MoveLeftCommand : AbstractCommand
+    {
+        private readonly CommandBuilder _commandBuilder=new();
+
+        public MoveLeftCommand(string id="2", string name="왼쪽으로 이동", int enhancedLevel=1, int grade=1)
+        : base(id, name, enhancedLevel, grade)
+        {
+        }
+
+        public override ICommand Copy(bool keepStatus = true)
+        {
+            if(!keepStatus)
+            {
+                return new MoveLeftCommand();
+            }
+            return new MoveLeftCommand(Id, Info.Name, Info.EnhancedLevel, Info.Grade);
+        }
+
+        public override IList<IStatement> GetCommandStatements(IRobotDelegate robot)
+        {
+            _commandBuilder.Clear();
+            _commandBuilder.Append(new MoveSidewaysStatement(robot, -1));
+            return _commandBuilder.Build();
+        }
+
+        public override bool Invoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Revoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
