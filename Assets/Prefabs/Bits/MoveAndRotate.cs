a58114cc9a6f3d0710af61ee94293b1da8ac3a26
[BIACKC4T] <[joshuafirenze@naver.com]>
2024-05-20 22:41:59 +0900

    Bit 설치

--- Assets/Prefabs/Bits/MoveAndRotate.cs
+++ Assets/Prefabs/Bits/MoveAndRotate.cs
@@ -1,7 +1,6 @@
+using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class MoveAndRotate : MonoBehaviour
 {
@@ -9,24 +8,29 @@ public class MoveAndRotate : MonoBehaviour
     public float moveSpeed = 0.6f; // 얼만큼 빠르게
     public float moveRange = 0.3f; // 얼만큼 이동
 
+    public float fallDuration = 1f;
+    public float fallDistance = 10f;
+    public bool checkanimate = false;
     private Vector3 startPosition; // 오브젝트 위치 지점 저장
     [SerializeField] AudioSource musicsource;
 
     private void Start()
     {
-        startPosition = transform.position; // 생성된 위치 저장
-        musicsource.time = 0.3f; // 음악의 0.3초부터 시작
+        StartCoroutine(AnimateItem());
     }
 
 //비트를 생성했을 때 회전 밑 아이템 이동이 
     private void Update()
     {
-        // Rotate around the z-axis at the specified speed
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
 
-        // Move up and down within the specified range at the specified speed
-        float newY = startPosition.y + Mathf.Sin(Time.time * moveSpeed) * moveRange;
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        if (checkanimate == true) {
+            // Rotate around the z-axis at the specified speed
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+
+            // Move up and down within the specified range at the specified speed
+            float newY = startPosition.y + Mathf.Sin(Time.time * moveSpeed) * moveRange;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
     }
 
 // 비트를 먹게 될 경우 실행시키면 되는 함수입니다.
@@ -38,5 +42,23 @@ public class MoveAndRotate : MonoBehaviour
 
         musicsource.Play();
         Destroy(gameObject, 0.47f);
+    }
+
+    IEnumerator AnimateItem()
+    {
+        startPosition = transform.position; // 생성된 위치 저장
+        musicsource.time = 0.3f; // 음악의 0.3초부터 시작s
+
+        // 동시에 시작할 애니메이션들을 담은 Sequence를 생성합니다.
+        Sequence sequence = DOTween.Sequence();
+
+        //
+        Vector3 endPosition = transform.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z);
+        sequence.Insert(0, transform.DOMove(endPosition, fallDuration).SetEase(Ease.OutCubic));
+
+        // Sequence를 시작합니다.
+        yield return sequence.WaitForCompletion();
+        checkanimate = true;
     }
 }
