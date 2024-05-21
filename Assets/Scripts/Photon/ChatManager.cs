51ba44f23adca4079ffa61e36fb9a8749f393650
gpfl0712 <gpfl0712@gmail.com>
2024-05-22 02:06:05 +0900

    Update ChatManager.cs

--- Assets/Scripts/Photon/ChatManager.cs
+++ Assets/Scripts/Photon/ChatManager.cs
@@ -1,56 +1,131 @@
+using ExitGames.Client.Photon;
+using Photon.Chat;
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Chat;
-using ExitGames.Client.Photon;
-using Photon.Pun;
+using UnityEngine.UI;
 
 public class ChatManager : MonoBehaviour, IChatClientListener
 {
+	#region Setup
+
 	ChatClient chatClient;
-	[SerializeField] string userID;
+	bool isConnected;
+	[SerializeField] string username;
+
+	public void UsernameOnValueChange(string valueIn)
+	{
+		username = valueIn;
+	}
+
+	#endregion Setup
+
+	#region General
+
+	[SerializeField] GameObject chatPanel;
+	string privateReceiver = "";
+	string currentChat;
+	[SerializeField] InputField chatField;
+	[SerializeField] Text chatDisplay;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		isConnected = true;
 		chatClient = new ChatClient(this);
-		chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(userID));
+		//chatClient.ChatRegion = "US";
+		chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(username));
+		Debug.Log("Connenting");
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		chatClient.Service();
+		if (isConnected)
+		{
+			chatClient.Service();
+		}
+
+		if (chatField.text != "" && Input.GetKey(KeyCode.Return))
+        {
+			SubmitPublicChatOnClick();
+		}
 	}
+
+	#endregion General
+
+	#region PublicChat
+
+	public void SubmitPublicChatOnClick()
+	{
+		if (privateReceiver == "")
+		{
+			chatClient.PublishMessage("RegionChannel", currentChat);
+			chatField.text = "";
+			currentChat = "";
+		}
+	}
+
+	public void TypeChatOnValueChange(string valueIn)
+	{
+		currentChat = valueIn;
+	}
+
+	#endregion PublicChat
+
+	#region Callbacks
 
 	public void DebugReturn(DebugLevel level, string message)
 	{
-		throw new System.NotImplementedException();
+		//throw new System.NotImplementedException();
 	}
 
 	public void OnChatStateChange(ChatState state)
 	{
-		throw new System.NotImplementedException();
+		if (state == ChatState.Uninitialized)
+		{
+			isConnected = false;
+			chatPanel.SetActive(false);
+		}
 	}
 
 	public void OnConnected()
 	{
-		throw new System.NotImplementedException();
+		Debug.Log("Connected");
+		chatClient.Subscribe(new string[] { "RegionChannel" });
 	}
 
 	public void OnDisconnected()
 	{
-		throw new System.NotImplementedException();
+		isConnected = false;
+		chatPanel.SetActive(false);
 	}
 
 	public void OnGetMessages(string channelName, string[] senders, object[] messages)
 	{
-		throw new System.NotImplementedException();
+		string msgs = "";
+		for (int i = 0; i < senders.Length; i++)
+        {
+			msgs = string.Format("{0}: {1}", senders[i], messages[i]);
+
+			chatDisplay.text += "\n" + msgs;
+
+			Debug.Log(msgs);
+		}
+
 	}
 
 	public void OnPrivateMessage(string sender, object message, string channelName)
 	{
-		throw new System.NotImplementedException();
+		string msgs = "";
+
+		msgs = string.Format("(Private) {0}: {1}", sender, message);
+
+		chatDisplay.text += "\n " + msgs;
+
+		Debug.Log(msgs);
+
 	}
 
 	public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
@@ -60,7 +135,7 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 
 	public void OnSubscribed(string[] channels, bool[] results)
 	{
-		throw new System.NotImplementedException();
+		chatPanel.SetActive(true);
 	}
 
 	public void OnUnsubscribed(string[] channels)
@@ -78,5 +153,5 @@ public class ChatManager : MonoBehaviour, IChatClientListener
 		throw new System.NotImplementedException();
 	}
 
-
+	#endregion Callbacks
 }
