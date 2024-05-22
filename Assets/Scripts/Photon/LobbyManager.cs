9c63c255ca87a4cfe8754e5ba7a9481324ca536e
hs-2171008-junhankim <115786692+hs-2171008-junhankim@users.noreply.github.com>
2024-05-23 00:51:25 +0900

    update GameLobby Scene
    아직 스크립트 적용x, 랜덤 참가/방참여 버튼 누를 시 모드 설명 부분 변경 등

--- Assets/Scripts/Photon/LobbyManager.cs
+++ Assets/Scripts/Photon/LobbyManager.cs
@@ -1,92 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
+using TMPro;
+using static UnityEngine.Rendering.DebugUI;
 
-
-public class LobbyManager : MonoBehaviourPunCallbacks
+public class LobbyManager : MonoBehaviour
 {
-    /*
-    public Button joinRoomButton;
-    public TextMeshProUGUI nicknameText;
-    public RawImage randomImage;
-    public GameObject roomEnterScroll; // 방 목록을 보여주는 스크롤 뷰
-    public GameObject roomEntryPrefab; // 방 항목을 표현하는 프리팹
-    public RoomInfo[] rooms;
+    public GameObject RandomRoomDescribe;
+    public GameObject StandardRoomDescribe;
+    public TMP_Text RoomTitle;
+    public TMP_Text UserCounts;
+    public Button RandomEnterBtn;
+
+    public void CreateRoom() => PhotonNetwork.CreateRoom(RoomTitle.text == "방 제목" ? "Room" + Random.Range(0, 100) : RoomTitle.text, new RoomOptions { MaxPlayers = 4 });
+
+    public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
+
+    // 랜덤 참가 버튼을 눌렀을 때
+    public void RandomRoomEnter()
+    {
+        StandardRoomDescribe.SetActive(false);
+        RandomRoomDescribe.SetActive(true);
+    }
+
+    // 생성되어 있는 방을 눌렀을 때
+    public void CreatedRoomEnter()
+    {
+        RandomRoomDescribe.SetActive(false);
+        StandardRoomDescribe.SetActive(true);
+    }
+
+    // 게임시작 버튼을 눌렀을 때
+    public void EnterGame()
+    {
+
+        SceneManager.LoadScene("GameScene");
+    }
+
 
     void Start()
     {
-        // 닉네임 설정
-        nicknameText.text = PhotonNetwork.NickName;
-
-        // 버튼 이벤트 설정
-        joinRoomButton.onClick.AddListener(OnJoinRoomButtonClick);
-        randomRoomButton.onClick.AddListener(OnRandomRoomButtonClick);
-
-        randomImage.gameObject.SetActive(false); // 초기 상태는 비활성화
+        RandomEnterBtn.onClick.AddListener(RandomRoomEnter());
     }
 
-    public void OnJoinRoomButtonClick()
+    // Update is called once per frame
+    void Update()
     {
-        // 현재 활성화된 방에 입장
-        foreach (RoomEntry entry in roomEnterScroll.GetComponentsInChildren<RoomEntry>())
-        {
-            // 만약 "Standard" 이미지가 활성화된 방이 있다면 해당 방에 입장합니다.
-            if (entry.standardImage.isActiveAndEnabled)
-            {
-                PhotonNetwork.JoinRoom(entry.roomInfo.Name);
-                break;
-            }
-        }
+
     }
-
-    // 방 목록이 업데이트될 때 호출되는 콜백 
-    public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    {
-        rooms = roomList.ToArray();
-
-        // 방 목록 UI를 업데이트합니다.
-        foreach (Transform child in roomEnterScroll.transform)
-        {
-            Destroy(child.gameObject);
-        }
-
-        foreach (var room in rooms)
-        {
-            GameObject roomEntryObject = Instantiate(roomEntryPrefab, roomEnterScroll.transform);
-            RoomEntry roomEntry = roomEntryObject.GetComponent<RoomEntry>();
-            roomEntry.SetRoomInfo(room);
-        }
-    }
-
-    // 랜덤 방 참여에 실패했을 때 호출되는 콜백
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        // 새로운 방을 생성합니다.
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = 4; // 방의 최대 참여자를 4명으로 설정
-        PhotonNetwork.CreateRoom(null, roomOptions, TypedLobby.Default);
-    }
-
-    // 방에 입장했을 때 호출되는 콜백
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("Joined Room");
-        // GameRoomScene으로 전환
-        SceneManager.LoadScene("GameRoomScene");
-    }
-
-    // 다른 플레이어가 방을 나갔을 때 호출되는 콜백
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        // 다른 플레이어가 방을 나갔을 때 그 방이 비어 있는지 확인하고, 비어 있다면 그 방을 파괴합니다.
-        foreach (RoomEntry entry in roomEnterScroll.GetComponentsInChildren<RoomEntry>())
-        {
-            entry.CheckDestroyRoom();
-        }
-    }
-    */
 }
