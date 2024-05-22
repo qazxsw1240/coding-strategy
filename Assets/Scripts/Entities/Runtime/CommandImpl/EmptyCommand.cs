f965e34b3fe648aa20d94a7fe77301e3b6f8b60b
es-e2b <ensu1997@naver.com>
2024-05-22 22:23:27 +0900

    Add EmptyCommand

---
+++ Assets/Scripts/Entities/Runtime/CommandImpl/EmptyCommand.cs
@@ -0,0 +1,44 @@
+#nullable enable
+
+
+namespace CodingStrategy.Entities.Runtime.CommandImpl
+{
+    using System.Collections.Generic;
+    using CodingStrategy.Entities.Robot;
+    using Statement;
+
+    public class EmptyCommand : AbstractCommand
+    {
+        private readonly CommandBuilder _commandBuilder=new();
+
+        public EmptyCommand(string id="0", string name="빈 커맨드", int enhancedLevel=1, int grade=1)
+        : base(id, name, enhancedLevel, grade)
+        {
+        }
+
+        public override ICommand Copy(bool keepStatus = true)
+        {
+            if(!keepStatus)
+            {
+                return new EmptyCommand();
+            }
+            return new EmptyCommand(Id, Info.Name, Info.EnhancedLevel, Info.Grade);
+        }
+
+        public override IList<IStatement> GetCommandStatements(IRobotDelegate robot)
+        {
+            _commandBuilder.Clear();
+            return _commandBuilder.Build();
+        }
+
+        public override bool Invoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Revoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
