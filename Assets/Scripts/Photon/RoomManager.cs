91b9291637bb544b56b0662d255af7a190485e90
[BIACKC4T] <[joshuafirenze@naver.com]>
2024-05-24 21:07:11 +0900

    Roommanager Ready system update

--- Assets/Scripts/Photon/RoomManager.cs
+++ Assets/Scripts/Photon/RoomManager.cs
@@ -34,10 +34,12 @@ public class RoomManager : MonoBehaviourPunCallbacks
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
             //playerNicknames의 0번째에 playerStates 클래스 내에 정의된 playersinRoom의 i번째의 유저의 닉네임을 가져옵니다.
-            if(PhotonNetwork.PlayerList[i].NickName!=null)
+            //근데 PlayerList[i]의 값이 Null이 아니야! >> 그러면 이제 그 위치의 닉네임 변경해주는겁니다.
+            if (PhotonNetwork.PlayerList[i].NickName!=null)
             { 
                 playerNicknames[i].text = PhotonNetwork.PlayerList[i].NickName;
                 
+                // 그리고 그 PlayerList의 customProperties의 Hashtage "isReady"의 값을 받아와서, 그게 true일 경우 "준비 완료!"를 띄울겁니다.
                 bool isReady = PhotonNetwork.PlayerList[i].CustomProperties.ContainsKey("isReady")
                        && (bool)PhotonNetwork.PlayerList[i].CustomProperties["isReady"];
 
@@ -51,13 +53,15 @@ public class RoomManager : MonoBehaviourPunCallbacks
                     playerReady[i].text = "준비 안함";
                 }
 
-                //만약에 해당 플레이어가 마스터 클라이언트(방장)이라면
+                //만약에 해당 플레이어가 마스터 클라이언트(방장)이라면 레디고 뭐고 방장인걸 표시해야겠죠?
                 if (PhotonNetwork.PlayerList[i].IsMasterClient)
                 {
                     playerReady[i].gameObject.SetActive(false); // playerReady[i] 비활성화
                     Master[i].gameObject.SetActive(true); // Master[i] 활성화
                 }
             }
+            //만약에 PhotonNetwork.PlayerList[i].NickName의 닉네임이 Null이라면 그 자리는 지금 비었다는 뜻이니까
+            //else를 치게 될 경우 (없음)과 "--"를 치게 될거에요.
             else
             {
                 playerNicknames[i].text = "(없음)";
@@ -80,20 +84,33 @@ public class RoomManager : MonoBehaviourPunCallbacks
         }
     }
 
+    private void ResetPlayerArrays()
+    {
+        // 모든 텍스트 필드를 초기 상태로 리셋합니다.
+        for (int i = 0; i < playerNicknames.Length; i++)
+        {
+            playerNicknames[i].text = "(없음)";
+            playerReady[i].text = "--";
+            Master[i].gameObject.SetActive(false);
+        }
+    }
+
     public void LateUpdate()
     {
         // 모든 플레이어가 준비 상태인지 확인합니다.
         foreach (TextMeshProUGUI readyCheck in playerReady)
         {
-           if (readyCheck.text != "준비 완료!" && readyCheck.text != "없음")
+           //한명이라도 준비 안함 상태라는 상태일 경우
+            if (readyCheck.text == "준비 안함")
            {
-                StopCoroutine(StartButtonCountdown());
+                StopCoroutine(StartButtonCountdown()); 
                 return;
            }
-           else 
-           {
-                StartCoroutine(StartButtonCountdown());
-           }
+            else //모두가 스타트 버튼을 누른 상태일 경우 StartButtonCountdown() 실행
+            {
+                StopCoroutine(StartButtonCountdown());
+                return;
+            }
         }
     }
 
@@ -108,6 +125,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         base.OnPlayerLeftRoom(otherPlayer);
+        ResetPlayerArrays();
         UpdatePlayerNicknames();
     }
 
@@ -119,7 +137,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
         // 방장을 강퇴하고 닉네임을 "없음"으로 변경
         if (PhotonNetwork.IsMasterClient)
         {
-            playerStates.playersinRoom.Remove(PhotonNetwork.LocalPlayer);
+            PhotonNetwork.CloseConnection(PhotonNetwork.LocalPlayer);
             UpdatePlayerNicknames();
         }
     }
@@ -140,9 +158,14 @@ public class RoomManager : MonoBehaviourPunCallbacks
         // 모든 플레이어가 준비 상태인지 확인합니다.
         foreach (TextMeshProUGUI readyCheck in playerReady)
         {
-            if (readyCheck.text != "준비 완료!" && readyCheck.text !="없음")
+            if (readyCheck.text == "준비 안함")
             {
                 Debug.Log("모든 플레이어가 준비 상태가 아닙니다.");
+                return;
+            }
+            else if (PhotonNetwork.PlayerList.Length < 2)
+            {
+                Debug.Log("1명이서는 게임이 불가능합니다.");
                 return;
             }
         }
