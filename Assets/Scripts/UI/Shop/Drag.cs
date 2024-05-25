8a12f35b4ea7c144a8db9d55d7237b278451262a
gpfl0712 <gpfl0712@gmail.com>
2024-05-25 20:46:37 +0900

    Update Drag & Drop

--- Assets/Scripts/UI/Shop/Drag.cs
+++ Assets/Scripts/UI/Shop/Drag.cs
@@ -7,13 +7,20 @@ namespace CodingStrategy.UI.Shop
     public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
         [SerializeField] private Image _image;
-        public Vector3 _oldPosition;
+        private Vector3 _oldPosition;
+        private Transform _oldParent;
+        private int _oldIndex;
+        private Transform alwaysOntop;
+        private Transform _tmpObject;
 
         // Start is called before the first frame update
         void Start()
         {
             _image = GetComponent<Image>();
             _oldPosition = _image.rectTransform.position;
+            _oldParent = transform.parent;
+            _oldIndex = transform.GetSiblingIndex();
+			alwaysOntop = GameObject.Find("AlwaysOnTop").transform;
         }
 
         // Update is called once per frame
@@ -27,17 +34,25 @@ namespace CodingStrategy.UI.Shop
         public void OnBeginDrag(PointerEventData eventData)
         {
             _image.raycastTarget = false;
+            _tmpObject = new GameObject().AddComponent<RectTransform>();
+            _tmpObject.SetParent(transform.parent);
+            _tmpObject.SetSiblingIndex(_oldIndex);
+            transform.SetParent(alwaysOntop);
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
-            transform.position = _oldPosition - new Vector3(Screen.width, 0, 0);
+            ResetPosition();
+            Debug.Log("OnEndDrag " + name + " " + eventData.pointerDrag.name);
             _image.raycastTarget = true;
-        }
+			transform.SetParent(_oldParent);
+            transform.SetSiblingIndex(_oldIndex);
+            Destroy(_tmpObject.gameObject);
+		}
 
-        public void SetNewPosition(Vector3 newPosition)
+        public void ResetPosition()
         {
-            _oldPosition = newPosition;
-        }
+			transform.position = _oldPosition - new Vector3(Screen.width * GameObject.Find("Shop Scroll View").GetComponent<ScrollRect>().horizontalNormalizedPosition, 0, 0);
+		}
     }
 }
--- Assets/Scripts/UI/Shop/Drop.cs
+++ Assets/Scripts/UI/Shop/Drop.cs
@@ -1,29 +1,48 @@
+using CodingStrategy.UI.InGame;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.UI;
 
 namespace CodingStrategy.UI.Shop
 {
     public class Drop : MonoBehaviour, IDropHandler
     {
         public string slotName;
+        private Sprite emptySprite;
 
         public void OnDrop(PointerEventData eventData)
         {
-            if (eventData.pointerDrag.transform.name == slotName)
+            if (eventData.pointerDrag.name == slotName)
             {
-                Drag draggable = eventData.pointerDrag.GetComponent<Drag>();
-                if (draggable != null)
+                if (slotName == "ShopItem")
                 {
-                    draggable._oldPosition = eventData.position + new Vector2(Screen.width, 0);
+                    Drag draggable = eventData.pointerDrag.GetComponent<Drag>();
+                    if (draggable == null) return;
+                    Debug.Log("Ondrop Buy index " + draggable.transform.GetSiblingIndex() + " to " + transform.GetSiblingIndex());
+                    transform.GetChild(0).GetComponent<Image>().sprite = draggable.transform.GetChild(0).GetComponent<Image>().sprite;
+                    transform.name = "SelectedItem";
+                    transform.AddComponent<Drag>();
+                    //Destroy(GetComponent<Drop>());
+                    slotName = "SelectedItem";
+                    //draggable.ResetPosition();
                 }
-            }
+                else if (slotName == "SelectedItem")
+                {
+					Drag draggable = eventData.pointerDrag.GetComponent<Drag>();
+                    if (draggable == null) return;
+					Debug.Log("Ondrop Sell index " + draggable.transform.GetSiblingIndex());
+				}
+			}
         }
 
         // Start is called before the first frame update
-        void Start() {}
+        void Start()
+        {
+            ;
+        }
 
         // Update is called once per frame
         void Update() {}
