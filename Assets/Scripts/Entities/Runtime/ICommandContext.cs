16c2fa257b46480172175622d12a048f616a6e43
qazxsw1240 <qazxsw010528@gmail.com>
2024-05-27 01:22:31 +0900

    Add ICommandContext

---
+++ Assets/Scripts/Entities/Runtime/ICommandContext.cs
@@ -0,0 +1,19 @@
+#nullable enable
+
+using CodingStrategy.Entities.Board;
+using CodingStrategy.Entities.Player;
+using CodingStrategy.Entities.Robot;
+
+namespace CodingStrategy.Entities.Runtime
+{
+    public interface ICommandContext
+    {
+        public abstract IBoardDelegate BoardDelegate { get; }
+        public abstract IRobotDelegatePool RobotDelegatePool { get; }
+        public abstract IPlayerPool PlayerPool { get; }
+
+        public abstract IRobotDelegate RobotDelegate { get; }
+        public abstract IPlayerDelegate PlayerDelegate { get; }
+
+    }
+}
