8a754b19626c146aeed636385848967b95eae6db
es-e2b <ensu1997@naver.com>
2024-05-28 02:09:57 +0900

    Update CoinMiningCommand

--- Assets/Scripts/Entities/Runtime/CommandImpl/CoinMiningCommand.cs
+++ Assets/Scripts/Entities/Runtime/CommandImpl/CoinMiningCommand.cs
@@ -4,15 +4,17 @@
 namespace CodingStrategy.Entities.Runtime.CommandImpl
 {
     using System.Collections.Generic;
+    using CodingStrategy.Entities.Player;
+    using CodingStrategy.Entities.Runtime.Abnormality;
     using Robot;
     using Statement;
 
     public class CoinMiningCommand : AbstractCommand
     {
-        private readonly CommandBuilder _commandBuilder=new();
-
+        private readonly int _coefficient;
+        private readonly IPlayerDelegate[] _target=new IPlayerDelegate[1];
         public CoinMiningCommand(string id="11", string name="코인 채굴", int enhancedLevel=1, int grade=4)
-        : base(id, name, enhancedLevel, grade)
+        : base(id, name, enhancedLevel, grade, 0)
         {
         }
 
@@ -27,11 +29,8 @@ namespace CodingStrategy.Entities.Runtime.CommandImpl
 
         public override IList<IStatement> GetCommandStatements(IRobotDelegate robot)
         {
-            _commandBuilder.Clear();
-            _commandBuilder.Append(new AddCurrencyByStackStatement(robot, 1));
-            if(Info.EnhancedLevel>=2)
-                _commandBuilder.Append(new AddCurrencyByStackStatement(robot, 1));
-            return _commandBuilder.Build();
+            _target[0]=Context?.PlayerDelegate!;
+            return base.GetCommandStatements(robot);
         }
 
         public override bool Invoke(params object[] args)
@@ -42,6 +41,21 @@ namespace CodingStrategy.Entities.Runtime.CommandImpl
         public override bool Revoke(params object[] args)
         {
             throw new System.NotImplementedException();
+        }
+
+        protected override void AddStatementOnLevel1(IRobotDelegate robotDelegate)
+        {
+            _commandBuilder.Append(new AddCurrencyTargetPlayerStatement(robotDelegate, _target, _coefficient, Stack.Name));
+        }
+
+        protected override void AddStatementOnLevel2(IRobotDelegate robotDelegate)
+        {
+            _commandBuilder.Append(new AddCurrencyTargetPlayerStatement(robotDelegate, _target, _coefficient, Stack.Name));
+        }
+
+        protected override void AddStatementOnLevel3(IRobotDelegate robotDelegate)
+        {
+            _commandBuilder.AppendFirst(new SuperStatement(robotDelegate));
         }
     }
 }
