8cd6cc0b217585e1375e9778e70ea6329d9c8220
gpfl0712 <gpfl0712@gmail.com>
2024-05-27 23:42:54 +0900

    Add OnclickDetail script

---
+++ Assets/Scripts/UI/GameScene/OnClickDetail.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodingStrategy.UI.InGame
+{
+    public class OnClickDetail : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
