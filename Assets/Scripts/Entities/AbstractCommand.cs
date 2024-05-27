92e1e57f71f41103d7745158ffa6bd123bac4a53
es-e2b <ensu1997@naver.com>
2024-05-28 01:51:07 +0900

    Update AbstractCoomand
    Add energy point and update GetCommandStatements

--- Assets/Scripts/Entities/AbstractCommand.cs
+++ Assets/Scripts/Entities/AbstractCommand.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using CodingStrategy.Entities.Robot;
 using CodingStrategy.Entities.Runtime;
+using CodingStrategy.Entities.Runtime.CommandImpl;
 
 namespace CodingStrategy.Entities
 {
@@ -16,6 +17,8 @@ namespace CodingStrategy.Entities
         private string _id;
 
         private readonly ICommandInfo _info;
+        private readonly int _energyPoint;
+        protected readonly CommandBuilder _commandBuilder;
 
         /// <summary>
         /// 기본적인 명령어 정보를 생성합니다.
@@ -23,10 +26,13 @@ namespace CodingStrategy.Entities
         /// <param name="id">명령어의 ID입니다.</param>
         /// <param name="name">명령어의 이름입니다.</param>
         /// <param name="enhancedLevel">명령어의 강화 단계입니다.</param>
-        protected AbstractCommand(string id, string name, int enhancedLevel, int grade)
+        /// <param name="energyPoint">명령어 실행에 필요한 에너지입니다.</param>
+        protected AbstractCommand(string id, string name, int enhancedLevel, int grade, int energyPoint)
         {
             _id = id;
             _info = new CommandInfoImpl(name, enhancedLevel, grade);
+            _energyPoint = energyPoint;
+            _commandBuilder = new();
         }
 
         public virtual string Id
@@ -37,11 +43,27 @@ namespace CodingStrategy.Entities
 
         public virtual ICommandInfo Info => _info;
 
+        public ICommandContext? Context { get; set; }
+
         public abstract bool Invoke(params object[] args);
 
         public abstract bool Revoke(params object[] args);
 
-        public abstract IList<IStatement> GetCommandStatements(IRobotDelegate robot);
+        public virtual IList<IStatement> GetCommandStatements(IRobotDelegate robotDelegate)
+        {
+            _commandBuilder.Clear();
+            if(robotDelegate.EnergyPoint<_energyPoint)
+            {
+                return _commandBuilder.Build();
+            }
+            AddStatementOnLevel1(robotDelegate);
+            AddStatementOnLevel2(robotDelegate);
+            AddStatementOnLevel3(robotDelegate);
+            return _commandBuilder.Build();
+        }
+        protected abstract void AddStatementOnLevel1(IRobotDelegate robotDelegate);
+        protected abstract void AddStatementOnLevel2(IRobotDelegate robotDelegate);
+        protected abstract void AddStatementOnLevel3(IRobotDelegate robotDelegate);
 
         public abstract ICommand Copy(bool keepStatus = true);
     }
