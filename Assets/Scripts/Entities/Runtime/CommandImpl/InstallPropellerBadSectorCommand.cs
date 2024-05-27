69434ea5c6ac671e78f6bac09dad3db0e11a7051
es-e2b <ensu1997@naver.com>
2024-05-28 04:13:17 +0900

    Add InstallPropellerBadSectorCommand

---
+++ Assets/Scripts/Entities/Runtime/CommandImpl/InstallPropellerBadSectorCommand.cs
@@ -0,0 +1,64 @@
+#nullable enable
+
+
+namespace CodingStrategy.Entities.Runtime.CommandImpl
+{
+    using System.Collections.Generic;
+    using CodingStrategy.Entities.BadSector;
+    using CodingStrategy.Entities.Board;
+    using Robot;
+    using Statement;
+
+    public class InstallPropellerBadSectorCommand : AbstractCommand
+    {
+        public static int installNum=0;
+        private readonly List<Coordinate> _coordinates=new();
+
+        public InstallPropellerBadSectorCommand(string id="15", string name="추진체 설치", int enhancedLevel=1, int grade=1)
+        : base(id, name, enhancedLevel, grade, 1)
+        {
+        }
+
+        public override ICommand Copy(bool keepStatus = true)
+        {
+            if(!keepStatus)
+            {
+                return new InstallPropellerBadSectorCommand();
+            }
+            return new InstallPropellerBadSectorCommand(Id, Info.Name, Info.EnhancedLevel, Info.Grade);
+        }
+        public IBadSectorDelegate InstallPropellerBadSector(IBoardDelegate boardDelegate, IRobotDelegate robotDelegate)
+        {
+            return new PropellerBadSector(robotDelegate.Id+"-"+installNum++, boardDelegate, robotDelegate);
+        }
+
+        public override bool Invoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Revoke(params object[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void AddStatementOnLevel1(IRobotDelegate robotDelegate)
+        {
+            _coordinates.Add(new Coordinate(0,1));
+            _commandBuilder.Append(new PointerStatement(robotDelegate, InstallPropellerBadSector, _coordinates));
+        }
+
+        protected override void AddStatementOnLevel2(IRobotDelegate robotDelegate)
+        {
+            return;
+        }
+
+        protected override void AddStatementOnLevel3(IRobotDelegate robotDelegate)
+        {
+            _commandBuilder.Clear();
+            _coordinates.Add(new Coordinate(-1,1));
+            _coordinates.Add(new Coordinate(1,1));
+            _commandBuilder.Append(new PointerStatement(robotDelegate, InstallPropellerBadSector, _coordinates));
+        }
+    }
+}
