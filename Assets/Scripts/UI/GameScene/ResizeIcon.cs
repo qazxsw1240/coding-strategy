48ded72b471d3df71cc69279e49246d1bac28cee
gpfl0712 <gpfl0712@gmail.com>
2024-05-27 23:42:11 +0900

    Add namespace

--- Assets/Scripts/UI/GameScene/ResizeIcon.cs
+++ Assets/Scripts/UI/GameScene/ResizeIcon.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResizeIcon : MonoBehaviour
+namespace CodingStrategy.UI.InGame
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ResizeIcon : MonoBehaviour
     {
-        transform.GetChild(0).GetComponent<RectTransform>().sizeDelta *= transform.parent.GetComponent<GridLayoutGroup>().cellSize / new Vector2(200.0f, 200.0f);
-	}
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.GetChild(0).GetComponent<RectTransform>().sizeDelta *= transform.parent.GetComponent<GridLayoutGroup>().cellSize / new Vector2(200.0f, 200.0f);
+        }
 
-    // Update is called once per frame
-    void Update() { }
+        // Update is called once per frame
+        void Update() { }
+    }
 }
