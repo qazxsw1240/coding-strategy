e8d9183269ff5f5dc52d7a47c726d5cc959df579
gpfl0712 <gpfl0712@gmail.com>
2024-05-29 22:57:42 +0900

    Attack Area

--- Assets/Scripts/UI/GameScene/SetCommandDetail.cs
+++ Assets/Scripts/UI/GameScene/SetCommandDetail.cs
@@ -11,6 +11,8 @@ namespace CodingStrategy.UI.InGame
         public TMP_Text _commandName;
         public TMP_Text _commandDescription;
         public Image _attackRange;
+        public Sprite[] spriteList;
+        public enum AttackArea { None, Forward, Left, Right, Backward, Forward2, Forward3, Backward3, ForwardLeft, ForwardRight, Malware2, Malware3, Around };
         public string Id;
 
         void SetCommandName(string name)
@@ -27,58 +29,93 @@ namespace CodingStrategy.UI.InGame
         {
             switch (int.Parse(Id))
             {
-                case 1:
-                    //_attackRange.sprite = enhancedLevel == 1 ? sprite
-                    //    : enhancedLevel == 2 ? sprite
-                    //    : enhancedLevel == 3 ? sprite;
-					break;
+                case 0:
+					_attackRange.sprite = spriteList[(int)AttackArea.None];
+                    break;
+				case 1:
+                    _attackRange.sprite = spriteList[(int)AttackArea.Forward];
+                    break;
                 case 2:
+                    _attackRange.sprite = spriteList[(int)AttackArea.Left];
                     break;
                 case 3:
+                    _attackRange.sprite = spriteList[(int)AttackArea.Right];
                     break;
                 case 4:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 5:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 6:
+                    _attackRange.sprite = spriteList[(int)AttackArea.ForwardLeft];
                     break;
                 case 7:
+                    _attackRange.sprite = spriteList[(int)AttackArea.ForwardRight];
                     break;
                 case 8:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 9:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 10:
+                    _attackRange.sprite = spriteList[(int)(enhancedLevel == 1 ? AttackArea.Forward2 :
+                                                        enhancedLevel == 2 ? AttackArea.Malware2 :
+                                                        enhancedLevel == 3 ? AttackArea.Malware3 : AttackArea.None)];
                     break;
                 case 11:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 12:
+                    _attackRange.sprite = spriteList[(int)AttackArea.Around];
                     break;
                 case 13:
+                    _attackRange.sprite = spriteList[(int)(enhancedLevel == 1 || enhancedLevel == 2 ? AttackArea.Forward :
+                                                        enhancedLevel == 3 ? AttackArea.Forward3 : AttackArea.None)];
                     break;
                 case 14:
+                    _attackRange.sprite = spriteList[(int)(enhancedLevel == 1 || enhancedLevel == 2 ? AttackArea.Backward :
+                                                        enhancedLevel == 3 ? AttackArea.Backward3 : AttackArea.None)];
                     break;
                 case 15:
+                    _attackRange.sprite = spriteList[(int)(enhancedLevel == 1 || enhancedLevel == 2 ? AttackArea.Forward :
+                                                        enhancedLevel == 3 ? AttackArea.Forward3 : AttackArea.None)];
                     break;
                 case 16:
+                    _attackRange.sprite = spriteList[(int)(enhancedLevel == 1 || enhancedLevel == 2 ? AttackArea.Backward :
+                                                        enhancedLevel == 3 ? AttackArea.Backward3 : AttackArea.None)];
                     break;
                 case 17:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 18:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 19:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 20:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 case 21:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
+
                     break;
                 case 22:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
+
                     break;
                 case 23:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
+
                     break;
                 case 24:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
+
                     break;
                 case 25:
+                    _attackRange.sprite = spriteList[(int)AttackArea.None];
                     break;
                 default:
                     Debug.Log("Wrong Command Detail Id: " + Id);
