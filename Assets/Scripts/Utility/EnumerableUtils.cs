3ac9e037c52d31cb0cb9d333a1419ad7b0a80836
qazxsw1240 <qazxsw010528@gmail.com>
2024-06-16 11:06:09 +0900

    Rename  Enumerables.cs to EnumerableUtils.cs

--- Assets/Scripts/Utility/EnumerableUtils.cs
+++ Assets/Scripts/Utility/EnumerableUtils.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace CodingStrategy.Utility
 {
-    public static class Enumerables
+    public static class EnumerableUtils
     {
         public static IEnumerable<(int, T)> ToIndexed<T>(this IEnumerable<T> enumerable)
         {
