883da026d297c9c7c6822febea830c8555281c3b
qazxsw1240 <qazxsw010528@gmail.com>
2025-05-08 10:27:21 +0900

    Add CommandListFactory

---
+++ Assets/Scripts/CommandListFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using CodingStrategy.Entities;
+using CodingStrategy.Entities.Runtime.Command;
+
+namespace CodingStrategy
+{
+    public static class CommandListFactory
+    {
+        public static IEnumerable<ICommand> GetCommands => new List<ICommand> {
+            new ForwardMoveCommand(),
+            new LeftMoveCommand(),
+            new RightRightCommand(),
+            new LeftRotationCommand(),
+            new RightRotationCommand(),
+            new LeftForwardMoveCommand(),
+            new RightForwardMoveCommand(),
+            new StackAddCommand(),
+            new GlobalWormAddCommand(),
+            new MalwareInstallerCommand(),
+            new CoinMiningCommand(),
+            new BotnetsCommand(),
+            new SchanzeInstallerCommand(),
+            new PropellerInstallerCommand(),
+            new ForwardAttackCommand(),
+            new BackwardAttackCommand(),
+            new EnergyChargeCommand(),
+            new DashCommand(),
+            new EnergyStorageIncreaseCommand(),
+            new ReinforcementCommand(),
+            new SelfRepairCommand(),
+            new RestorationCommand()
+        };
+    }
+}
--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -1,49 +1,39 @@
+﻿using System.Collections.Generic;
+
 using CodingStrategy.Entities;
-using CodingStrategy.Entities.Runtime.Command;
 using CodingStrategy.Network;
 
 namespace CodingStrategy
 {
     public static class GameInitializer
     {
+        public static readonly List<int> StockCountsByCommandGrade = new List<int>() { 0, 128, 108, 72, 48, 36 };
+
         public const int DefaultGrade1CommandStockCount = 128;
         public const int DefaultGrade2CommandStockCount = 108;
         public const int DefaultGrade3CommandStockCount = 72;
         public const int DefaultGrade4CommandStockCount = 48;
         public const int DefaultGrade5CommandStockCount = 36;
 
-        public static void AddCommand(ICommand command, int count)
+        private static void AddCommand(ICommand command, int count)
         {
             string id = command.ID;
-
             PhotonPlayerCommandCache.AttachCommand(command);
             PhotonPlayerCommandNetworkDelegate.AttachCommandIdCount(id, count);
         }
 
+        public static void AddCommand(ICommand command)
+        {
+            int stockCount = StockCountsByCommandGrade[command.Info.Grade];
+            AddCommand(command, stockCount);
+        }
+
         public static void Initialize()
         {
-            AddCommand(new ForwardMoveCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new LeftMoveCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new RightRightCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new LeftRotationCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new RightRotationCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new LeftForwardMoveCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new RightForwardMoveCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new StackAddCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new GlobalWormAddCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new MalwareInstallerCommand(), DefaultGrade3CommandStockCount);
-            AddCommand(new CoinMiningCommand(), DefaultGrade4CommandStockCount);
-            AddCommand(new BotnetsCommand(), DefaultGrade5CommandStockCount);
-            AddCommand(new SchanzeInstallerCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new PropellerInstallerCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new ForwardAttackCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new BackwardAttackCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new EnergyChargeCommand(), DefaultGrade1CommandStockCount);
-            AddCommand(new DashCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new EnergyStorageIncreaseCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new ReinforcementCommand(), DefaultGrade2CommandStockCount);
-            AddCommand(new SelfRepairCommand(), DefaultGrade4CommandStockCount);
-            AddCommand(new RestorationCommand(), DefaultGrade5CommandStockCount);
+            foreach (ICommand command in CommandListFactory.GetCommands)
+            {
+                AddCommand(command);
+            }
         }
     }
 }
