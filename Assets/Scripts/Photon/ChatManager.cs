3e3dc25529c9d584d65f705e62bced1e63b319a4
qazxsw1240 <qazxsw010528@gmail.com>
2025-05-08 13:57:55 +0900

    Update chat input field

--- Assets/Scripts/Photon/ChatManager.cs
+++ Assets/Scripts/Photon/ChatManager.cs
@@ -1,126 +1,43 @@
-using System;
-using System.Collections;
+﻿using System;
 
 using ExitGames.Client.Photon;
 
-using Photon.Chat;
 using Photon.Pun;
 using Photon.Realtime;
 
-using TMPro;
-
-using UnityEngine;
-using UnityEngine.UI;
-
 namespace CodingStrategy.Photon
 {
+    public readonly struct ChatMessage
+    {
+        public ChatMessage(string sender, string message)
+        {
+            Sender = sender;
+            Message = message;
+        }
+
+        public string Sender { get; }
+
+        public string Message { get; }
+    }
+
     public class ChatManager : MonoBehaviourPunCallbacks, IOnEventCallback
     {
         private const byte EventCode = 198;
 
-        private const string _announceChannel = "Announce";
-        private static readonly string _roomChannel = "Room";
+        public event Action<ChatMessage> OnChatMessageReceived;
 
-        [SerializeField]
-        public TMP_InputField chatField;
-
-        [SerializeField]
-        public TMP_Text chatDisplay;
-
-        [SerializeField]
-        private string currentAnnounceChannel;
-
-        [SerializeField]
-        private string currentRoomChannel;
-
-        [SerializeField]
-        private string[] currentChannels;
-
-        [Obsolete]
-        private readonly string privateReceiver = "";
-
-        [Obsolete]
-        private ChatClient _chatClient;
-
-        private bool _isConnected;
-        private string currentChat;
-
-        public void Awake()
+        public void Send(string message)
         {
-            if (!chatField)
+            if (!PhotonNetwork.InRoom || string.IsNullOrWhiteSpace(message))
             {
-                throw new UnassignedReferenceException(nameof(chatField));
+                return;
             }
-            if (!chatDisplay)
-            {
-                throw new UnassignedReferenceException(nameof(chatDisplay));
-            }
-            chatField.onValueChanged.AddListener(TypeChatOnValueChange);
-            Button button = gameObject.GetComponentInChildren<Button>();
-            if (!button)
-            {
-                throw new UnassignedReferenceException(nameof(button));
-            }
-            button.onClick.AddListener(SubmitPublicChatOnClick);
-        }
-
-        public IEnumerator Start()
-        {
-            yield return new WaitUntil(() => !PhotonNetwork.InRoom);
-
-            // _chatClient = new ChatClient(this) { ChatRegion = "kr" };
-            // currentAnnounceChannel = _announceChannel + PhotonNetwork.NetworkingClient.CurrentRoom;
-            // currentRoomChannel = _roomChannel + PhotonNetwork.NetworkingClient.CurrentRoom;
-            // currentChannels = new[] { currentAnnounceChannel, currentRoomChannel };
-            // string appIdChat = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat;
-            // string appVersion = PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion;
-            // AuthenticationValues authenticationValues =
-            //     new AuthenticationValues(PhotonNetwork.NetworkingClient.NickName);
-            // _chatClient.Connect(appIdChat, appVersion, authenticationValues);
-        }
-
-        public void Update()
-        {
-            if (chatField.text != "" && Input.GetKey(KeyCode.Return))
-            {
-                SubmitPublicChatOnClick();
-            }
-        }
-
-        public void DebugReturn(DebugLevel level, string message) {}
-
-        public void OnChatStateChange(ChatState state)
-        {
-            if (state == ChatState.Uninitialized)
-            {
-                _isConnected = false;
-            }
+            SendMessage(PhotonNetwork.LocalPlayer.NickName, message);
         }
 
         public void Announce(string message)
         {
-            // _chatClient.PublishMessage(_announceChannel, message);
             SendMessage("시스템", message);
-        }
-
-        public void SubmitPublicChatOnClick()
-        {
-            // if (privateReceiver == "")
-            // {
-            //     // _chatClient.PublishMessage(_roomChannel, currentChat);
-            // }
-            if (!PhotonNetwork.InRoom)
-            {
-                return;
-            }
-            SendMessage(PhotonNetwork.LocalPlayer.NickName, currentChat);
-            chatField.text = "";
-            currentChat = "";
-        }
-
-        public void TypeChatOnValueChange(string valueIn)
-        {
-            currentChat = valueIn;
         }
 
         public void OnEvent(EventData photonEvent)
@@ -129,10 +46,8 @@ namespace CodingStrategy.Photon
             {
                 return;
             }
-            (string sender, string message) = ParseMessage(photonEvent.CustomData);
-            Debug.LogFormat("Received Message from {1}: {0}", message, sender);
-            string chat = $"{sender}: {message}";
-            chatDisplay.text += "\n" + chat;
+            ChatMessage chatMessage = ParseMessage(photonEvent.CustomData);
+            OnChatMessageReceived?.Invoke(chatMessage);
         }
 
         private static void SendMessage(string sender, string message)
@@ -148,7 +63,7 @@ namespace CodingStrategy.Photon
                 SendOptions.SendReliable);
         }
 
-        private static (string, string) ParseMessage(object data)
+        private static ChatMessage ParseMessage(object data)
         {
             if (data is not object[] tuple)
             {
@@ -162,7 +77,7 @@ namespace CodingStrategy.Photon
             {
                 throw new ArgumentException();
             }
-            return (sender, message);
+            return new ChatMessage(sender, message);
         }
     }
 }
---
+++ Assets/Scripts/Photon/ChatRenderer.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodingStrategy.Photon
+{
+    public class ChatRenderer : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_InputField _chatField;
+
+        [SerializeField]
+        private TMP_Text _chatDisplay;
+
+        [SerializeField]
+        private Button _sendButton;
+
+        private ChatManager _chatManager;
+
+        private void Awake()
+        {
+            if (!_chatField)
+            {
+                throw new UnassignedReferenceException("Cannot find chat field");
+            }
+            if (!_chatDisplay)
+            {
+                throw new UnassignedReferenceException("Cannot find chat display");
+            }
+            _chatManager = GetComponent<ChatManager>();
+            if (!_chatManager)
+            {
+                throw new UnassignedReferenceException("Cannot find chat manager");
+            }
+        }
+
+        private void Start()
+        {
+            _sendButton.onClick.AddListener(OnSendButtonPressed);
+            _chatManager.OnChatMessageReceived += OnChatMessageReceived;
+        }
+
+        private void OnDestroy()
+        {
+            _sendButton.onClick.RemoveListener(OnSendButtonPressed);
+            _chatManager.OnChatMessageReceived -= OnChatMessageReceived;
+        }
+
+        private void OnSendButtonPressed()
+        {
+            _chatManager.Send(_chatField.text);
+            _chatField.text = "";
+        }
+
+        private void OnChatMessageReceived(ChatMessage message)
+        {
+            _chatDisplay.text += "\n" + $"{message.Sender}: {message.Message}";
+        }
+    }
+}
